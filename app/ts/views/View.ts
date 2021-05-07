92b1d204ad83e48cc323710ff4a2793dfeab012b
ElenAlmeida <elenalmeida@outlook.com>
2021-05-07 12:24:30 -0300

    first commit

---
+++ app/ts/views/View.ts
@@ -0,0 +1,16 @@
+abstract class View<T>{
+
+  protected _element: Element;
+
+  constructor(seletor: string) {
+
+    this._element = document.querySelector(seletor);
+  }
+
+  update(model:T): void{
+    this._element.innerHTML = this.template(model);
+  }
+
+  abstract template (model: T): string;
+
+}
